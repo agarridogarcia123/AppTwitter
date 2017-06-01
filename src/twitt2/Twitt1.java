3695d0014a69b2c740ae877155bbe19319ef52b5
agarridogarcia <agarridogarcia@nautilus5.danielcastelao.org>
2017-06-01 09:02:30 +0200

    novo modo para conectarse a twitter e ver o timeline

--- src/twitt2/Twitt1.java
+++ src/twitt2/Twitt1.java
@@ -9,6 +9,7 @@ import twitt2.Metodos;
 import twitter4j.DirectMessage;
 import twitter4j.Query;
 import twitter4j.QueryResult;
+import twitter4j.ResponseList;
 import twitter4j.Status;
 import twitter4j.Twitter;
 import twitter4j.TwitterException;
@@ -24,25 +25,46 @@ public class Twitt1 {
     /**
      * @param args menú de twitter
      */
-    public static void main(String[] args) {
-        Metodos.acceso();
-        int opcion;
+    public static void main(String[] args) throws TwitterException{
+        
+        ConfigurationBuilder cb = new ConfigurationBuilder();
 
-        do { 
-            opcion = Integer.parseInt(JOptionPane.showInputDialog("1. Novo estado \n 2.TimeLine \n 3.Buscar un twitt \n 4.Mensaxe Privada "));
-            switch (opcion) {
+        TwitterFactory tf = new TwitterFactory(cb.build());
 
-                case 1:Metodos.twitear("Hola");
-                    break;
-                case 2:Metodos.timeLine();
-                    break;
-                case 3:Metodos.buscarTt("hola");
-                    break;
-                case 4:Metodos.enviarMP(JOptionPane.showInputDialog("Introduce destinatario"),JOptionPane.showInputDialog("Introduce a mensaxe"));
-                    break;
-                
-            }
+        Twitter twitter = tf.getInstance();
 
-        } while (opcion != 0);
+        ResponseList <twitter4j.Status> statuses= twitter.getHomeTimeline();
+
+        System.out.println("Mostrando el timeline...");
+
+        for (twitter4j.Status status : statuses) {
+
+            System.out.println("@" + status.getUser().getScreenName()+": "+ status.getText());
+
+           
+
+        }
+
     }
-}
+    }
+//        Metodos.acceso();
+//        int opcion;
+//
+//        do { 
+//            opcion = Integer.parseInt(JOptionPane.showInputDialog("1. Novo estado \n 2.TimeLine \n 3.Buscar un twitt \n 4.Mensaxe Privada "));
+//            switch (opcion) {
+//
+//                case 1:Metodos.twitear("Hola");
+//                    break;
+//                case 2:Metodos.timeLine();
+//                    break;
+//                case 3:Metodos.buscarTt("hola");
+//                    break;
+//                case 4:Metodos.enviarMP(JOptionPane.showInputDialog("Introduce destinatario"),JOptionPane.showInputDialog("Introduce a mensaxe"));
+//                    break;
+//                
+//            }
+//
+//        } while (opcion != 0);
+//    }
+//}
